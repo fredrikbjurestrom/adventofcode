d296007ef3b221ca3def213e7f218d89e9f1af66
Fredrik Bjureström <fredrik.bjurestrom@nok.se>
2019-12-02 15:11:34 +0100

    Added Day1 part2

--- 2019/day01.js
+++ 2019/day01.js
@@ -1,4 +1,22 @@
 const fs = require('fs');
 
-var array = fs.readFileSync("day01input.txt").toString().split("\n");;
-console.log(array.filter(x => x !== '').reduce((acc, cur) => {return acc+(Math.floor(cur/3)-2)}, 0));
+const calculateNeededFuel = (mass) => {
+  return Math.floor(mass/3)-2;
+};
+
+const calculateNeededFuelIteratively = (mass) => {
+    var totalFuel = 0;
+    var fuel = calculateNeededFuel(mass);
+    while(fuel > 0)
+    {
+        totalFuel += fuel;
+        fuel = calculateNeededFuel(fuel);
+    }
+    return totalFuel;
+}
+
+var array = fs.readFileSync("day01input.txt").toString().split("\n");
+
+
+console.log("Part 1: " + array.filter(x => x !== '').reduce((acc, cur) => {return acc + calculateNeededFuel(cur)}, 0));
+console.log("Part 2: " + array.filter(x => x !== '').reduce((acc, cur) => {return acc + calculateNeededFuelIteratively(cur)}, 0));
