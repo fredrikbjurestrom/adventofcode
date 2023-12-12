04563342b5b96d36917dc4dbeede5833fc89498b
Fredrik Bjureström <fredrik.bjurestrom@nok.se>
2023-12-12 09:43:45 +0100

    Dag 11, på riktigt

--- python/2023/day11.py
+++ python/2023/day11.py
@@ -2,38 +2,41 @@ from pathlib import Path
 import numpy as np
 import itertools
 
-
 with open(Path(__file__).parent / "inputs/day11.txt") as file:
     grid = np.array([[c for c in row] for row in file.read().splitlines()])
+    galaxies = np.array([g for g in np.argwhere(grid == "#")])
 
-def expand_space(expansion):
-    space = grid.copy()
-    insert_y = 0
-    for match in np.all(space[:, 0:] == ".", axis=0):
-        if match:
-            for _ in range(1, expansion):
-                space = np.insert(space, insert_y, ".", axis=1)
-                insert_y += 1
 
-        insert_y += 1
+def expand_space(unexpanded_space, expansion):
+    """
+    First we find all space rows and columns (all ".":s)
+    Then we iterate each galaxy and increment the x and y value by an incrementing offset of the expansion
+    """
+    space_columns = np.all(unexpanded_space[:, 0:] == ".", axis=0)
+    x_offset = 0
+    for space_x in np.arange(grid.shape[1])[space_columns]:
+        for galaxy in galaxies:
+            if galaxy[1] > space_x + x_offset:
+                galaxy[1] += expansion - 1
+        x_offset += expansion - 1
 
-    insert_x = 0
-    for match in np.all(space[:, 0:] == ".", axis=1):
-        if match:
-          for _ in range(1, expansion):
-            space = np.insert(space, insert_x, ".", axis=0)
-            insert_x += 1
-        insert_x += 1
-
-    galaxies = [tuple(g) for g in np.argwhere(space == "#")]
+    space_rows = np.all(grid[:, 0:] == ".", axis=1)
+    y_offset = 0
+    for space_y in np.arange(grid.shape[0])[space_rows]:
+        for galaxy in galaxies:
+            if galaxy[0] > space_y + y_offset:
+                galaxy[0] += expansion - 1
+        y_offset += expansion - 1
 
     return galaxies
 
 
-    
-def part1(galaxies) -> int:
+def calculate_distance(expansion: int) -> int:
+    """Manhattan distance between two points"""
+    expanded_galaxies = expand_space(grid, expansion)
+
     distance = 0
-    for from_galaxy, to_galaxy in itertools.combinations(galaxies, 2):
+    for from_galaxy, to_galaxy in itertools.combinations(expanded_galaxies, 2):
         ydiff, xdiff = np.subtract(from_galaxy, to_galaxy)
         distance += abs(ydiff) + abs(xdiff)
 
@@ -41,5 +44,5 @@ def part1(galaxies) -> int:
 
 
 if __name__ == "__main__":
-    print("Part 1:", part1(expand_space(2)))
-    # print("Part 2:", part1(expand_space(1_000_000)))
+    print("Part 1:", calculate_distance(expansion=2))
+    print("Part 2:", calculate_distance(expansion=1_000_000))
