662468dd118ebf06fff733b0289a4d81ec4e4d6e
furobaz <fbjure@gmail.com>
2025-12-06 15:22:47 +0100

    day 4: del 1

---
+++ cs/2025/day04.cs
@@ -0,0 +1,41 @@
+#!/usr/bin/dotnet run
+
+#:project ./lib/lib.csproj
+
+using static Lib.IO;
+
+var grid = ReadInput("./inputs/day04.txt")
+    .Select(row => row.ToArray())
+    .ToArray();
+
+var part1 = grid.SelectMany(
+            (line, y) => line.Where((pos, x) => pos == '@' && accessibleByForklift(grid, y, x))
+        )
+        .Count();
+
+Console.WriteLine($"Part 1: {part1}, Part 2: {part1}");
+
+bool accessibleByForklift(char[][] grid, int y, int x)
+{
+    var papers = 0;
+    var neighbours = Enumerable.Range(-1, 3).ToArray();
+
+    foreach (var ymov in neighbours)
+    {
+        var neighbourY = y + ymov;
+        if (neighbourY < 0 || neighbourY >= grid.Length)
+            continue;
+
+        foreach (var xmov in neighbours.Where(x => ymov != 0 || x != 0))
+        {
+            var neighbourX = x + xmov;
+            if (neighbourX < 0 || neighbourX >= grid[neighbourY].Length)
+                continue;
+
+            if (grid[neighbourY][neighbourX] == '@')
+                papers += 1;
+        }
+    }
+
+    return papers < 4;
+}
